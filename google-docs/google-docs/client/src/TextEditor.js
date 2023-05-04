fd552ef49fd939a94b00fb62a3fa82138942a503
Khushi Harsure <78033927+snoice45@users.noreply.github.com>
2023-05-04 16:47:58 +0530

    Update TextEditor.js

--- google-docs/google-docs/client/src/TextEditor.js
+++ google-docs/google-docs/client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("http://localhost:3001")
+    const s = io("http://localhost:2000")
     setSocket(s)
 
     return () => {
