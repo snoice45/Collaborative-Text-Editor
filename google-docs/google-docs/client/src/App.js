482ef9b7d527aeab89b0fee47310064f6d6250f3
Khushi Harsure <78033927+snoice45@users.noreply.github.com>
2023-06-03 20:08:37 +0530

    Update App.js
    Added google sign-in and ui changes

--- google-docs/google-docs/client/src/App.js
+++ google-docs/google-docs/client/src/App.js
@@ -1,4 +1,5 @@
 import TextEditor from "./TextEditor"
+import "./styles.css"
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,12 +7,46 @@ import {
   Redirect,
 } from "react-router-dom"
 import { v4 as uuidV4 } from "uuid"
+import {auth,provider} from "./FirebaseConfig";
+import {useState ,useEffect} from "react";
+import { signInWithPopup } from "firebase/auth";
 
 function App() {
+
+  const [user,setUser]=useState();
+  const signIn=()=>{
+    signInWithPopup(auth,provider).then((result)=>{
+      setUser(result.user);
+    }).catch((error)=>{
+      alert(error.message);
+    })
+  }
+
   return (
+    <div>
+    {user ? (
+      <>
+      <div class="text"><h3>Welcome {user.displayName} !</h3> </div>
+            
     <Router>
       <Switch>
+
         <Route path="/" exact>
+      
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router> </>):(  <div><div>
+        <button class="Login" type="button" onClick={signIn}>Sign In With Google</button>   </div>
+        
+        <Router>
+      <Switch>
+
+        <Route path="/" exact>
+      
           <Redirect to={`/documents/${uuidV4()}`} />
         </Route>
         <Route path="/documents/:id">
@@ -19,6 +54,11 @@ function App() {
         </Route>
       </Switch>
     </Router>
+     
+    </div>    
+        )}
+ 
+    </div>
   )
 }
 
